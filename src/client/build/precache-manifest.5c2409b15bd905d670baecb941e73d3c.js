9c1368730ea05253c151557e8266450ecf0671d6
JulianHinsch <jhinsch799@gmail.com>
2019-06-25 17:42:16 -0400

    build client

---
+++ src/client/build/precache-manifest.5c2409b15bd905d670baecb941e73d3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a161f2d70ca84abd98307538c4ed3536",
+    "url": "/index.html"
+  },
+  {
+    "revision": "300acb09617d7daa667e",
+    "url": "/static/css/main.289a3ed1.chunk.css"
+  },
+  {
+    "revision": "6260ec73e778a2481351",
+    "url": "/static/js/2.7085d921.chunk.js"
+  },
+  {
+    "revision": "300acb09617d7daa667e",
+    "url": "/static/js/main.0d85c4de.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9a79fb6493cc74e0c812b269523dcbae",
+    "url": "/static/media/home.9a79fb64.svg"
+  },
+  {
+    "revision": "ba50176fcb5ecd79a9e6cef6df06a617",
+    "url": "/static/media/logout.ba50176f.svg"
+  }
+]);
