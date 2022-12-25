6ec5c7ad7e89dffa69951bb47b8c0aa844d3fba2
Julian Hinsch <jhinsch799@gmail.com>
2022-12-25 15:26:32 -0500

    refactor NotFound component

--- src/client/src/ui/components/NotFound/NotFound.js
+++ src/client/src/ui/components/NotFound/NotFound.js
@@ -2,13 +2,11 @@ import React from "react";
 
 import styles from "./NotFound.module.scss";
 
-const NotFound = () => {
-  return (
-    <main className={styles.not_found}>
-      <h1>404!</h1>
-      <p>The page you're looking for can't be found.</p>
-    </main>
-  );
-};
+const NotFound = () => (
+  <main className={styles.not_found}>
+    <h1>404!</h1>
+    <p>The page you're looking for can't be found.</p>
+  </main>
+);
 
 export default NotFound;
